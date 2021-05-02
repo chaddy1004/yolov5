65adc78da5f0165a89ce7528f7d4b4d7f4b25974
chaddy <cpaik1004@gmail.com>
2021-05-01 21:40:46 -0400

    wip on getting prediction working the way i want it to

--- test_trained_model.py
+++ test_trained_model.py
@@ -1,29 +1,160 @@
 from models.yolo import Model
 from models.experimental import attempt_load
 import cv2
-from torchvision import transforms
+import torchvision
 import numpy as np
 from utils.torch_utils import select_device, load_classifier, time_synchronized
 import torch
-
+import random
 from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
     scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
+import time
 
-weights = 'yolov5s.pt'
+from glob import glob as glob
 
-model = attempt_load(weights, map_location='cpu')
-img = cv2.imread('test.jpg')
-img = cv2.resize(img, (640, 640), interpolation=cv2.INTER_AREA)
-
-img_tensor = transforms.ToTensor()(img)
-print(img_tensor.shape)
-img_tensor = img_tensor.unsqueeze(0)
-bbox = model(img_tensor)
-print(len(bbox))
-print(bbox[0].shape)
+if torch.cuda.device_count() > 0:
+    print("RUNNING ON GPU")
+    DEVICE = torch.device('cuda')
+else:
+    print("RUNNING ON CPU")
+    DEVICE = torch.device('cpu')
 
 
-def detect(save_img=False):
+def xywh2xyxy(x):
+    # Convert nx4 boxes from [x, y, w, h] to [x1, y1, x2, y2] where xy1=top-left, xy2=bottom-right
+    y = x.clone() if isinstance(x, torch.Tensor) else np.copy(x)
+    y[:, 0] = x[:, 0] - x[:, 2] / 2  # top left x
+    y[:, 1] = x[:, 1] - x[:, 3] / 2  # top left y
+    y[:, 2] = x[:, 0] + x[:, 2] / 2  # bottom right x
+    y[:, 3] = x[:, 1] + x[:, 3] / 2  # bottom right y
+    return y
+
+
+def non_max_suppression(prediction, conf_thres=0.25, iou_thres=0.45, classes=None, agnostic=False, multi_label=False,
+                        labels=()):
+    """Runs Non-Maximum Suppression (NMS) on inference results
+
+    Returns:
+         list of detections, on (n,6) tensor per image [xyxy, conf, cls]
+    """
+
+    nc = prediction.shape[2] - 5  # number of classes
+    xc = prediction[..., 4] > conf_thres  # candidates
+
+    # Settings
+    min_wh, max_wh = 2, 4096  # (pixels) minimum and maximum box width and height
+    max_det = 300  # maximum number of detections per image
+    max_nms = 30000  # maximum number of boxes into torchvision.ops.nms()
+    time_limit = 10.0  # seconds to quit after
+    redundant = True  # require redundant detections
+    multi_label &= nc > 1  # multiple labels per box (adds 0.5ms/img)
+    merge = False  # use merge-NMS
+
+    t = time.time()
+    output = [torch.zeros((0, 6), device=prediction.device)] * prediction.shape[0]
+    for xi, x in enumerate(prediction):  # image index, image inference
+        # Apply constraints
+        # x[((x[..., 2:4] < min_wh) | (x[..., 2:4] > max_wh)).any(1), 4] = 0  # width-height
+        x = x[xc[xi]]  # confidence
+
+        # Cat apriori labels if autolabelling
+        if labels and len(labels[xi]):
+            l = labels[xi]
+            v = torch.zeros((len(l), nc + 5), device=x.device)
+            v[:, :4] = l[:, 1:5]  # box
+            v[:, 4] = 1.0  # conf
+            v[range(len(l)), l[:, 0].long() + 5] = 1.0  # cls
+            x = torch.cat((x, v), 0)
+
+        # If none remain process next image
+        if not x.shape[0]:
+            continue
+
+        # Compute conf
+        x[:, 5:] *= x[:, 4:5]  # conf = obj_conf * cls_conf
+
+        # Box (center x, center y, width, height) to (x1, y1, x2, y2)
+        box = xywh2xyxy(x[:, :4])
+
+        # Detections matrix nx6 (xyxy, conf, cls)
+        if multi_label:
+            i, j = (x[:, 5:] > conf_thres).nonzero(as_tuple=False).T
+            x = torch.cat((box[i], x[i, j + 5, None], j[:, None].float()), 1)
+        else:  # best class only
+            conf, j = x[:, 5:].max(1, keepdim=True)
+            x = torch.cat((box, conf, j.float()), 1)[conf.view(-1) > conf_thres]
+
+        # Filter by class
+        if classes is not None:
+            x = x[(x[:, 5:6] == torch.tensor(classes, device=x.device)).any(1)]
+
+        # Apply finite constraint
+        # if not torch.isfinite(x).all():
+        #     x = x[torch.isfinite(x).all(1)]
+
+        # Check shape
+        n = x.shape[0]  # number of boxes
+        if not n:  # no boxes
+            continue
+        elif n > max_nms:  # excess boxes
+            x = x[x[:, 4].argsort(descending=True)[:max_nms]]  # sort by confidence
+
+        # Batched NMS
+        c = x[:, 5:6] * (0 if agnostic else max_wh)  # classes
+        boxes, scores = x[:, :4] + c, x[:, 4]  # boxes (offset by class), scores
+        i = torchvision.ops.nms(boxes, scores, iou_thres)  # NMS
+        if i.shape[0] > max_det:  # limit detections
+            i = i[:max_det]
+        if merge and (1 < n < 3E3):  # Merge NMS (boxes merged using weighted mean)
+            # update boxes as boxes(i,4) = weights(i,n) * boxes(n,4)
+            iou = box_iou(boxes[i], boxes) > iou_thres  # iou matrix
+            weights = iou * scores[None]  # box weights
+            x[i, :4] = torch.mm(weights, x[:, :4]).float() / weights.sum(1, keepdim=True)  # merged boxes
+            if redundant:
+                i = i[iou.sum(1) > 1]  # require redundancy
+
+        output[xi] = x[i]
+        if (time.time() - t) > time_limit:
+            print(f'WARNING: NMS time limit {time_limit}s exceeded')
+            break  # time limit exceeded
+
+    return output
+
+
+def letterbox(img, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleFill=False, scaleup=True, stride=32):
+    # Resize and pad image while meeting stride-multiple constraints
+    shape = img.shape[:2]  # current shape [height, width]
+    if isinstance(new_shape, int):
+        new_shape = (new_shape, new_shape)
+
+    # Scale ratio (new / old)
+    r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
+    if not scaleup:  # only scale down, do not scale up (for better test mAP)
+        r = min(r, 1.0)
+
+    # Compute padding
+    ratio = r, r  # width, height ratios
+    new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
+    dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]  # wh padding
+    if auto:  # minimum rectangle
+        dw, dh = np.mod(dw, stride), np.mod(dh, stride)  # wh padding
+    elif scaleFill:  # stretch
+        dw, dh = 0.0, 0.0
+        new_unpad = (new_shape[1], new_shape[0])
+        ratio = new_shape[1] / shape[1], new_shape[0] / shape[0]  # width, height ratios
+
+    dw /= 2  # divide padding into 2 sides
+    dh /= 2
+
+    if shape[::-1] != new_unpad:  # resize
+        img = cv2.resize(img, new_unpad, interpolation=cv2.INTER_LINEAR)
+    top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
+    left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
+    img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
+    return img, ratio, (dw, dh)
+
+
+def detect(model, test_path, classes, save_img=False):
     weights = 'yolov5.pt'
     img_size = 640
 
@@ -45,47 +176,36 @@ def detect(save_img=False):
 
     # Set Dataloader
     vid_path, vid_writer = None, None
-    if webcam:
-        view_img = check_imshow()
-        cudnn.benchmark = True  # set True to speed up constant image size inference
-        dataset = LoadStreams(source, img_size=imgsz, stride=stride)
-    else:
-        save_img = True
-        dataset = LoadImages(source, img_size=imgsz, stride=stride)
 
     # Get names and colors
     names = model.module.names if hasattr(model, 'module') else model.names
     colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
 
     # Run inference
-    if device.type != 'cpu':
-        model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
+    if DEVICE.type != 'cpu':
+        model(torch.zeros(1, 3, img_size, img_size).to(DEVICE).type_as(next(model.parameters())))  # run once
     t0 = time.time()
-    for path, img, im0s, vid_cap in dataset:
-        img = torch.from_numpy(img).to(device)
-        img = img.half() if half else img.float()  # uint8 to fp16/32
-        img /= 255.0  # 0 - 255 to 0.0 - 1.0
-        if img.ndimension() == 3:
-            img = img.unsqueeze(0)
+    imgs = glob(test_path)
+    for img_file in imgs:
+        # the model takes in RGB
+        im0 = cv2.imread(img_file)
+        img = cv2.imread(img_file)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        # the resizing technique used by this project
+        img = letterbox(img, (img_size, img_size), stride=32)  # using the default values
+        # img /= 255.0 # normalization
+        # change (HxWxC) -> (CxHxW)
+        img_tensor = torchvision.transforms.ToTensor()(img)  # this takes care of normalization as well. I did not know that
+        img_tensor = img_tensor.half() if half else img_tensor.float()  # uint8 to fp16/32
+        if img_tensor.ndimension() == 3:
+            img_tensor = img_tensor.unsqueeze(0)
 
-        # Inference
-        t1 = time_synchronized()
-        pred = model(img, augment=opt.augment)[0]
+        pred = model(img_tensor, augment=False)[0] # returns y, None
 
-        # Apply NMS
-        pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, classes=opt.classes, agnostic=opt.agnostic_nms)
-        t2 = time_synchronized()
-
-        # Apply Classifier
-        if classify:
-            pred = apply_classifier(pred, modelc, img, im0s)
+        pred = non_max_suppression(pred, conf_thres=0.25, iou_thres=0.45, classes=classes, agnostic=False)
 
         # Process detections
         for i, det in enumerate(pred):  # detections per image
-            if webcam:  # batch_size >= 1
-                p, s, im0, frame = path[i], '%g: ' % i, im0s[i].copy(), dataset.count
-            else:
-                p, s, im0, frame = path, '', im0s, getattr(dataset, 'frame', 0)
 
             p = Path(p)  # to Path
             save_path = str(save_dir / p.name)  # img.jpg
@@ -113,33 +233,88 @@ def detect(save_img=False):
                         label = f'{names[int(cls)]} {conf:.2f}'
                         plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
 
-            # Print time (inference + NMS)
-            print(f'{s}Done. ({t2 - t1:.3f}s)')
-
-            # Stream results
-            if view_img:
-                cv2.imshow(str(p), im0)
-                cv2.waitKey(1)  # 1 millisecond
-
-            # Save results (image with detections)
-            if save_img:
-                if dataset.mode == 'image':
-                    cv2.imwrite(save_path, im0)
-                else:  # 'video'
-                    if vid_path != save_path:  # new video
-                        vid_path = save_path
-                        if isinstance(vid_writer, cv2.VideoWriter):
-                            vid_writer.release()  # release previous video writer
-
-                        fourcc = 'mp4v'  # output video codec
-                        fps = vid_cap.get(cv2.CAP_PROP_FPS)
-                        w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-                        h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-                        vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*fourcc), fps, (w, h))
-                    vid_writer.write(im0)
 
     if save_txt or save_img:
         s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt else ''
         print(f"Results saved to {save_dir}{s}")
 
     print(f'Done. ({time.time() - t0:.3f}s)')
+
+
+if __name__ == '__main__':
+    classes = ["Aluminium foil",
+     "Battery",
+     "Aluminium blister pack",
+     "Carded blister pack",
+     "Other plastic bottle",
+     "Clear plastic bottle",
+     "Glass bottle",
+     "Plastic bottle cap",
+     "Metal bottle cap",
+     "Broken glass",
+     "Food Can",
+     "Aerosol",
+     "Drink can",
+     "Toilet tube",
+     "Other carton",
+     "Egg carton",
+     "Drink carton",
+     "Corrugated carton",
+     "Meal carton",
+     "Pizza box",
+     "Paper cup",
+     "Disposable plastic cup",
+     "Foam cup",
+     "Glass cup",
+     "Other plastic cup",
+     "Food waste",
+     "Glass jar",
+     "Plastic lid",
+     "Metal lid",
+     "Other plastic",
+     "Magazine paper",
+     "Tissues",
+     "Wrapping paper",
+     "Normal paper",
+     "Paper bag",
+     "Plastified paper bag",
+     "Plastic film",
+     "Six pack rings",
+     "Garbage bag",
+     "Other plastic wrapper",
+     "Single-use carrier bag",
+     "Polypropylene bag",
+     "Crisp packet",
+     "Spread tub",
+     "Tupperware",
+     "Disposable food container",
+     "Foam food container",
+     "Other plastic container",
+     "Plastic glooves",
+     "Plastic utensils",
+     "Pop tab",
+     "Rope & strings",
+     "Scrap metal",
+     "Shoe",
+     "Squeezable tube",
+     "Plastic straw",
+     "Paper straw",
+     "Styrofoam piece",
+     "Unlabeled litter",
+     "Cigarette"
+     ]
+    weights = 'yolov5s.pt'
+
+    model = attempt_load(weights, map_location='cpu')
+    img = cv2.imread('test.jpg')
+    img = cv2.resize(img, (640, 640), interpolation=cv2.INTER_AREA)
+
+    img_tensor = torchvision.transforms.ToTensor()(img)
+    print(img_tensor.shape)
+    img_tensor = img_tensor.unsqueeze(0)
+    bbox = model(img_tensor)
+    print(len(bbox))
+    print(bbox[0].shape)
+
+    test_path = ""
+    detect(model=model, test_path=test_path, classes=classes, save_img=True)
